eec4367ce73157d78ae917d50edbeb666e0d6c19
classickiller <classickillertech@gmail.com>
2020-02-17 17:03:01 +0530

    Adding Addition

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 
 		System.out.println("***LOG: Start of Main");
 		System.out.println("Hello Git Hub");
+		System.out.println(5+7);
 		System.out.println("***LOG: End of Main");
 	}
 
